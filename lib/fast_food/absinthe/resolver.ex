c74e43584a351462a20b3fb1093d765a6d457662
Marcin Lewandowski <kontakt@marcin-lewandowski.com>
2024-02-22 11:07:57 +0100

    Return validation errors in extensions field

--- lib/fast_food/absinthe/resolver.ex
+++ lib/fast_food/absinthe/resolver.ex
@@ -7,7 +7,10 @@ defmodule FastFood.Absinthe.Resolver do
   require Logger
 
   def resolve_root_query_many(ecto_schema, parent, args, resolution) do
-    Logger.debug("[FastFood.Absinthe.Resolver] Root Query (many), ecto_schema = #{inspect(ecto_schema)}, parent = #{inspect(parent)}, args = #{inspect(args)}")
+    Logger.debug(
+      "[FastFood.Absinthe.Resolver] Root Query (many), ecto_schema = #{inspect(ecto_schema)}, parent = #{inspect(parent)}, args = #{inspect(args)}"
+    )
+
     %Absinthe.Blueprint.Document.Field{selections: selections} = resolution.definition
 
     @ecto_repo.transaction(fn ->
@@ -16,7 +19,10 @@ defmodule FastFood.Absinthe.Resolver do
   end
 
   def resolve_root_query_one(ecto_schema, parent, %{id: id} = args, resolution) do
-    Logger.debug("[FastFood.Absinthe.Resolver] Root Query (one), ecto_schema = #{inspect(ecto_schema)}, parent = #{inspect(parent)}, args = #{inspect(args)}")
+    Logger.debug(
+      "[FastFood.Absinthe.Resolver] Root Query (one), ecto_schema = #{inspect(ecto_schema)}, parent = #{inspect(parent)}, args = #{inspect(args)}"
+    )
+
     %Absinthe.Blueprint.Document.Field{selections: selections} = resolution.definition
 
     @ecto_repo.transaction(fn ->
@@ -25,7 +31,10 @@ defmodule FastFood.Absinthe.Resolver do
   end
 
   def resolve_root_create_one(ecto_schema, parent, %{input: input} = args, resolution) do
-    Logger.debug("[FastFood.Absinthe.Resolver] Root Create (one), ecto_schema = #{inspect(ecto_schema)}, parent = #{inspect(parent)}, args = #{inspect(args)}")
+    Logger.debug(
+      "[FastFood.Absinthe.Resolver] Root Create (one), ecto_schema = #{inspect(ecto_schema)}, parent = #{inspect(parent)}, args = #{inspect(args)}"
+    )
+
     %Absinthe.Blueprint.Document.Field{selections: selections} = resolution.definition
 
     @ecto_repo.transaction(fn ->
@@ -64,13 +73,17 @@ defmodule FastFood.Absinthe.Resolver do
   end
 
   def resolve_root_update_one(ecto_schema, parent, %{id: id, input: input} = args, resolution) do
-    Logger.debug("[FastFood.Absinthe.Resolver] Root Update (one), ecto_schema = #{inspect(ecto_schema)}, parent = #{inspect(parent)}, args = #{inspect(args)}")
+    Logger.debug(
+      "[FastFood.Absinthe.Resolver] Root Update (one), ecto_schema = #{inspect(ecto_schema)}, parent = #{inspect(parent)}, args = #{inspect(args)}"
+    )
+
     %Absinthe.Blueprint.Document.Field{selections: selections} = resolution.definition
 
     @ecto_repo.transaction(fn ->
       case @ecto_repo.get(ecto_schema, id) do
         nil ->
           @ecto_repo.rollback(:notfound)
+
         base ->
           changeset =
             if function_exported?(ecto_schema, :ff_changeset_update, 2) do
@@ -106,7 +119,10 @@ defmodule FastFood.Absinthe.Resolver do
   end
 
   def resolve_root_delete_one(ecto_schema, parent, %{id: id} = args, resolution) do
-    Logger.debug("[FastFood.Absinthe.Resolver] Root Delete (one), ecto_schema = #{inspect(ecto_schema)}, parent = #{inspect(parent)}, args = #{inspect(args)}")
+    Logger.debug(
+      "[FastFood.Absinthe.Resolver] Root Delete (one), ecto_schema = #{inspect(ecto_schema)}, parent = #{inspect(parent)}, args = #{inspect(args)}"
+    )
+
     %Absinthe.Blueprint.Document.Field{selections: selections} = resolution.definition
 
     @ecto_repo.transaction(fn ->
@@ -150,21 +166,30 @@ defmodule FastFood.Absinthe.Resolver do
         {:error, message: "Unable to find record with given ID", code: :notfound}
 
       {:error, changeset = %Ecto.Changeset{}} ->
-        # FIXME put this in extensions structure so it is machine readable
-        reason =
+        traversed =
           changeset
           |> Ecto.Changeset.traverse_errors(fn {msg, opts} ->
             Regex.replace(~r"%{(\w+)}", msg, fn _, key ->
               opts |> Keyword.get(String.to_existing_atom(key), key) |> to_string()
             end)
           end)
-          |> Enum.map(fn ({field, errors}) ->
-            # FIXME errors can be nested, e.g.
+
+        reason =
+          traversed
+          |> Enum.map(fn {field, errors} ->
+            # TODO errors can be nested, e.g.
             # %{a: %{b: [\"message\"]}})
-            "#{field} #{inspect(errors)}"
+            "#{field} #{Enum.join(errors, ", ")}"
           end)
           |> Enum.join("; ")
-        {:error, message: "Unable to perform database operation: (#{reason})", code: :validation}
+
+        fields =
+          traversed
+          |> Enum.into(%{})
+
+        {:error,
+         message: "Unable to perform database operation: #{reason}",
+         extensions: %{validation: fields}}
     end
   end
 
@@ -198,12 +223,21 @@ defmodule FastFood.Absinthe.Resolver do
   end
 
   # Association (many, nullable)
-  defp do_load_single(record, [%Absinthe.Blueprint.Document.Field{
-    selections: selections,
-    schema_node: %Absinthe.Type.Field{
-      identifier: field_identifier,
-      type: %Absinthe.Type.List{of_type: assoc_field_type}
-    }}|rest], acc) when is_atom(assoc_field_type) and length(selections) != 0 do
+  defp do_load_single(
+         record,
+         [
+           %Absinthe.Blueprint.Document.Field{
+             selections: selections,
+             schema_node: %Absinthe.Type.Field{
+               identifier: field_identifier,
+               type: %Absinthe.Type.List{of_type: assoc_field_type}
+             }
+           }
+           | rest
+         ],
+         acc
+       )
+       when is_atom(assoc_field_type) and length(selections) != 0 do
     # IO.puts "- do_load_single (assoc one nullable) record = #{print_record(record)}, field_identifier = #{inspect(field_identifier)}, acc = #{inspect(acc)}"
     assoc_data =
       record
@@ -214,16 +248,25 @@ defmodule FastFood.Absinthe.Resolver do
   end
 
   # Association (many, list non-nullable, items nullable)
-  defp do_load_single(record, [%Absinthe.Blueprint.Document.Field{
-    selections: selections,
-    schema_node: %Absinthe.Type.Field{
-      identifier: field_identifier,
-      type: %Absinthe.Type.NonNull{
-        of_type: %Absinthe.Type.List{
-          of_type: assoc_field_type
-        }
-      }
-    }}|rest], acc) when is_atom(assoc_field_type) and length(selections) != 0 do
+  defp do_load_single(
+         record,
+         [
+           %Absinthe.Blueprint.Document.Field{
+             selections: selections,
+             schema_node: %Absinthe.Type.Field{
+               identifier: field_identifier,
+               type: %Absinthe.Type.NonNull{
+                 of_type: %Absinthe.Type.List{
+                   of_type: assoc_field_type
+                 }
+               }
+             }
+           }
+           | rest
+         ],
+         acc
+       )
+       when is_atom(assoc_field_type) and length(selections) != 0 do
     # IO.puts "- do_load_single (assoc one nullable) record = #{print_record(record)}, field_identifier = #{inspect(field_identifier)}, acc = #{inspect(acc)}"
 
     assoc_data =
@@ -235,18 +278,27 @@ defmodule FastFood.Absinthe.Resolver do
   end
 
   # Association (many, list non-nullable, items non-nullable)
-  defp do_load_single(record, [%Absinthe.Blueprint.Document.Field{
-    selections: selections,
-    schema_node: %Absinthe.Type.Field{
-      identifier: field_identifier,
-      type: %Absinthe.Type.NonNull{
-        of_type: %Absinthe.Type.List{
-          of_type: %Absinthe.Type.NonNull{
-            of_type: assoc_field_type
-          }
-        }
-      }
-    }}|rest], acc) when is_atom(assoc_field_type) and length(selections) != 0 do
+  defp do_load_single(
+         record,
+         [
+           %Absinthe.Blueprint.Document.Field{
+             selections: selections,
+             schema_node: %Absinthe.Type.Field{
+               identifier: field_identifier,
+               type: %Absinthe.Type.NonNull{
+                 of_type: %Absinthe.Type.List{
+                   of_type: %Absinthe.Type.NonNull{
+                     of_type: assoc_field_type
+                   }
+                 }
+               }
+             }
+           }
+           | rest
+         ],
+         acc
+       )
+       when is_atom(assoc_field_type) and length(selections) != 0 do
     # IO.puts "- do_load_single (assoc one nullable) record = #{print_record(record)}, field_identifier = #{inspect(field_identifier)}, acc = #{inspect(acc)}"
 
     assoc_data =
@@ -258,40 +310,71 @@ defmodule FastFood.Absinthe.Resolver do
   end
 
   # Association (one, nullable)
-  defp do_load_single(record, [%Absinthe.Blueprint.Document.Field{
-    selections: selections,
-    schema_node: %Absinthe.Type.Field{
-      identifier: field_identifier,
-      type: assoc_field_type
-    }}|rest], acc) when is_atom(assoc_field_type) and length(selections) != 0 do
+  defp do_load_single(
+         record,
+         [
+           %Absinthe.Blueprint.Document.Field{
+             selections: selections,
+             schema_node: %Absinthe.Type.Field{
+               identifier: field_identifier,
+               type: assoc_field_type
+             }
+           }
+           | rest
+         ],
+         acc
+       )
+       when is_atom(assoc_field_type) and length(selections) != 0 do
     # IO.puts "- do_load_single (assoc one nullable) record = #{print_record(record)}, field_identifier = #{inspect(field_identifier)}, acc = #{inspect(acc)}"
     assoc_data = fetch_assoc_one(record, field_identifier)
     do_load_single(record, rest, Map.put(acc, field_identifier, assoc_data))
   end
 
   # Association (one, list non-nullable)
-  defp do_load_single(record, [%Absinthe.Blueprint.Document.Field{
-    selections: selections,
-    schema_node: %Absinthe.Type.Field{
-      identifier: field_identifier,
-      type: %Absinthe.Type.NonNull{of_type: assoc_field_type},
-    }}|rest], acc) when is_atom(assoc_field_type) and length(selections) != 0 do
-      # IO.puts "- do_load_single (assoc one non-nullable) record = #{print_record(record)}, field_identifier = #{inspect(field_identifier)}, acc = #{inspect(acc)}"
-      assoc_data = fetch_assoc_one(record, field_identifier)
-      do_load_single(record, rest, Map.put(acc, field_identifier, assoc_data))
+  defp do_load_single(
+         record,
+         [
+           %Absinthe.Blueprint.Document.Field{
+             selections: selections,
+             schema_node: %Absinthe.Type.Field{
+               identifier: field_identifier,
+               type: %Absinthe.Type.NonNull{of_type: assoc_field_type}
+             }
+           }
+           | rest
+         ],
+         acc
+       )
+       when is_atom(assoc_field_type) and length(selections) != 0 do
+    # IO.puts "- do_load_single (assoc one non-nullable) record = #{print_record(record)}, field_identifier = #{inspect(field_identifier)}, acc = #{inspect(acc)}"
+    assoc_data = fetch_assoc_one(record, field_identifier)
+    do_load_single(record, rest, Map.put(acc, field_identifier, assoc_data))
   end
 
   # Regular field
-  defp do_load_single(record, [%Absinthe.Blueprint.Document.Field{
-    selections: selections,
-    schema_node: %Absinthe.Type.Field{
-      identifier: field_identifier
-    }}|rest], acc) when length(selections) == 0 do
-      # IO.puts "- do_load_single (field) record = #{print_record(record)}, field_identifier = #{inspect(field_identifier)}, acc = #{inspect(acc)}"
-    do_load_single(record, rest, Map.put(acc, field_identifier, Map.get(record, field_identifier)))
+  defp do_load_single(
+         record,
+         [
+           %Absinthe.Blueprint.Document.Field{
+             selections: selections,
+             schema_node: %Absinthe.Type.Field{
+               identifier: field_identifier
+             }
+           }
+           | rest
+         ],
+         acc
+       )
+       when length(selections) == 0 do
+    # IO.puts "- do_load_single (field) record = #{print_record(record)}, field_identifier = #{inspect(field_identifier)}, acc = #{inspect(acc)}"
+    do_load_single(
+      record,
+      rest,
+      Map.put(acc, field_identifier, Map.get(record, field_identifier))
+    )
   end
 
-  defp do_load_single(record, [field|_rest], _acc) do
+  defp do_load_single(record, [field | _rest], _acc) do
     raise "TODO do_load_single #{inspect(record)} #{inspect(field)}"
   end
 
@@ -300,13 +383,13 @@ defmodule FastFood.Absinthe.Resolver do
     Enum.reverse(acc)
   end
 
-  defp do_load_collection([record|rest], selections, acc) do
+  defp do_load_collection([record | rest], selections, acc) do
     # IO.puts "- do_load_collection record = #{print_record(record)}, selections = #{inspect(selections)}, acc = #{inspect(acc)}"
     data =
       record
       |> do_load_single(selections, %{})
 
-    do_load_collection(rest, selections, [data|acc])
+    do_load_collection(rest, selections, [data | acc])
   end
 
   # Association helpers
@@ -360,7 +443,8 @@ defmodule FastFood.Absinthe.Resolver do
   # Customizations
 
   defp apply_filter_query_for_root(ecto_schema) do
-    queryable = (from ecto_schema)
+    queryable = from(ecto_schema)
+
     if function_exported?(ecto_schema, :ff_filter_query, 2) do
       ecto_schema.ff_filter_query(nil, queryable)
     else
@@ -384,6 +468,7 @@ defmodule FastFood.Absinthe.Resolver do
   defp apply_virtual_fields(record) when is_struct(record) do
     # TODO pass args etc.
     ecto_schema = record.__struct__
+
     if function_exported?(ecto_schema, :ff_virtual_values, 1) do
       ecto_schema.ff_virtual_values(record)
     else
